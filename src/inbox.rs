1a9e4c0cdb7c7fd01a6c8c498eaf45cb784022fe
Filip Trplan <info@trplan.si>
2025-11-08 00:30:30 +0100

    feat: add inbox new_tls function
    created new function to make an Inbox struct with a TLS connection
    to the IMAP server.

---
+++ src/inbox.rs
@@ -0,0 +1,54 @@
+use anyhow::Context;
+use chrono::prelude::*;
+use native_tls::TlsStream;
+use std::{
+    io::{Read, Write},
+    net::TcpStream,
+};
+
+use imap::Session;
+
+#[derive(Debug)]
+pub struct Inbox<T: Read + Write> {
+    /// The IMAP session that we use throughout the execution of the program
+    pub imap_session: Session<T>,
+    /// The date of the last fetch. Used to periodically fetch new messages.
+    last_fetch_date: DateTime<Local>,
+    /// The main folder in which the messages arrive.
+    folder: String,
+}
+
+impl Inbox<TlsStream<TcpStream>> {
+    /// Creates an `Inbox` using a `TlsConnector` using username/password credentials.
+    pub fn new_tls(domain: &str, port: u16, user: &str, pass: &str) -> anyhow::Result<Self> {
+        let tls = native_tls::TlsConnector::builder().build().unwrap();
+
+        // we pass in the domain twice to check that the server's TLS
+        // certificate is valid for the domain we're connecting to.
+        let client = imap::connect((domain, port), domain, &tls)
+            .with_context(|| "Failed to connect to IMAP server")?;
+
+        // the client we have here is unauthenticated.
+        // to do anything useful with the e-mails, we need to log in
+        let mut imap_session = client
+            .login(user, pass)
+            .map_err(|e| e.0)
+            .with_context(|| "Failed to login to IMAP")?;
+        let folder = String::from("INBOX");
+        imap_session
+            .select(&folder)
+            .with_context(|| format!("Failed to select folder {}", folder))?;
+
+        Ok(Inbox {
+            imap_session,
+            last_fetch_date: DateTime::from_timestamp_nanos(0).into(),
+            folder,
+        })
+    }
+}
+
+impl<T: Read + Write> Drop for Inbox<T> {
+    fn drop(&mut self) {
+        let _ = self.imap_session.logout();
+    }
+}
