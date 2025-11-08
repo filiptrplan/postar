7833ebb2130e84c7641fd77b2610bbe0894d86ec
Filip Trplan <info@trplan.si>
2025-11-08 11:19:15 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::env;
+
+use postar::inbox::Inbox;
+
 fn main() {
-    println!("Hello, world!");
+    let domain = env::var("DOMAIN").unwrap();
+    let user = env::var("USER").unwrap();
+    let pass = env::var("PASS").unwrap();
+    let port = env::var("PORT").unwrap().parse::<u16>().unwrap();
+
+    let inbox = Inbox::new_tls(&domain, port, &user, &pass);
+
+    if let anyhow::Result::Err(err) = inbox {
+        println!("{:?}", err);
+    }
+
+    // we want to fetch the first email in the INBOX mailbox
+    // imap_session.select("INBOX").unwrap();
+    //
+    // // fetch message number 1 in this mailbox, along with its RFC822 field.
+    // // RFC 822 dictates the format of the body of e-mails
+    // let messages = imap_session.fetch("1", "RFC822").unwrap();
+    // let message = if let Some(m) = messages.iter().next() {
+    //     m
+    // } else {
+    //     return;
+    // };
+    //
+    // // extract the message's body
+    // let body = message.body().expect("message did not have a body!");
+    // let body = std::str::from_utf8(body)
+    //     .expect("message was not valid utf-8")
+    //     .to_string();
+    //
+    // // be nice to the server and log out
+    // imap_session.logout().unwrap();
+    //
+    // let email = MessageParser::default().parse(&body).unwrap();
+    //
+    // println!("{:?}", email.body_text(0));
 }
