0e080e0407aff17bb31d555008c5df4b67866e1f
Filip Trplan <info@trplan.si>
2025-11-27 16:35:02 +0100

    feat: error reporting for lexer

--- src/dsl/lexer.rs
+++ src/dsl/lexer.rs
@@ -3,19 +3,8 @@ use logos::{Logos, Span};
 
 use crate::dsl::File;
 
-#[derive(Debug, Clone, Default, PartialEq)]
-pub enum LexingError {
-    MalformedString,
-    MalformedIdentfier,
-    InvalidSymbol,
-    InvalidKeyword,
-    #[default]
-    Other,
-}
-
 #[derive(Logos, Debug, PartialEq, Clone)]
 #[logos(skip r"[ \t\n\f]+")]
-#[logos(error = LexingError)]
 pub enum Token {
     // Definition keywords
     #[token("folder")]
@@ -84,31 +73,10 @@ pub enum Token {
     Colon,
 }
 
-fn handle_error(err: &LexingError, file: &File, span: &Span) {
-    let label_str = match err {
-        LexingError::MalformedString => "Malformed string",
-        LexingError::MalformedIdentfier => "Malformed identifier",
-        LexingError::InvalidSymbol => "Invalid symbol",
-        LexingError::InvalidKeyword => "Invalid keyword",
-        LexingError::Other => "Unknown error",
-    }
-    .to_string();
-    let label = Label::new((&file.file_name, span.clone())).with_message(label_str);
-    let note_str = match err {
-        LexingError::MalformedString => "Strings are defined the same as in JSON RFC 8259.",
-        LexingError::MalformedIdentfier => {
-            "Identfiers are snake_case. Only lowercase letters, numbers and underscores allowed."
-        }
-        LexingError::InvalidSymbol => "Valid symbols: {, }, [, ], (, ), :",
-        LexingError::InvalidKeyword => {
-            "Valid keywords: folder, rule, name, matcher, action, and, or, not, subject, to, from, body, startswith, contains, equals, regex, delete, moveto"
-        }
-        LexingError::Other => "Unknown error, please contact the developers.",
-    }.to_string();
+fn handle_error(file: &File, span: &Span) {
     Report::build(ReportKind::Error, (&file.file_name, span.clone()))
         .with_message("Syntax error.".to_string())
-        .with_label(label)
-        .with_note(note_str)
+        .with_label(Label::new((&file.file_name, span.clone())).with_message("Error detected here"))
         .finish()
         .print((&file.file_name, Source::from(&file.contents)))
         .unwrap();
@@ -120,8 +88,8 @@ pub fn process_tokens(file: &File) -> anyhow::Result<Vec<(Token, Span)>> {
     let mut syntax_errors = tokens.clone().filter(|(res, _)| res.is_err()).peekable();
 
     if syntax_errors.peek().is_some() {
-        syntax_errors.for_each(|(res, span)| {
-            handle_error(&res.unwrap_err(), file, &span);
+        syntax_errors.for_each(|(_, span)| {
+            handle_error(file, &span);
         });
     }
 
