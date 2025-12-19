00a7f7ee1bd14af8442c03f7120bd8acd925adb0
Filip Trplan <info@trplan.si>
2025-12-19 14:30:46 +0100

    feat: add tables for imap folders

--- src/migrations.rs
+++ src/migrations.rs
@@ -2,7 +2,23 @@ use rusqlite_migration::{M, Migrations};
 
 // 1️⃣ Define migrations
 const MIGRATIONS_SLICE: &[M<'_>] = &[
-    M::up("CREATE TABLE friend(name TEXT NOT NULL);"),
+    M::up(
+        "CREATE TABLE imap_servers(
+           id INTEGER PRIMARY KEY AUTOINCREMENT,
+           server TEXT NOT NULL,
+           user TEXT NOT NULL,
+           UNIQUE(server, user)
+        );
+        CREATE TABLE imap_folders(
+           server_id INTEGER NOT NULL,
+           name TEXT NOT NULL,
+           last_seen_uid INTEGER,
+           uid_validity INTEGER,
+           PRIMARY KEY(server_id, name),
+           FOREIGN KEY(server_id) REFERENCES imap_servers(id)
+        );
+",
+    ),
     // In the future, add more migrations here:
     //M::up("ALTER TABLE friend ADD COLUMN email TEXT;"),
 ];
